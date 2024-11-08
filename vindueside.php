a39d27d144e3f5a647c918d38781a9eb56db44b2
screirax <eqwow@hotmail.com>
2024-11-08 08:49:38 +0100

    test2

---
+++ vindueside.php
@@ -0,0 +1,77 @@
+<?php
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>ILVA</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    <?php include 'settings/links.php'?>
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<?php include 'settings/headertop.php';?>
+
+<div class="container-fluid">
+    <?php include 'settings/header.php';
+    include 'settings/menu.php'?>
+</div>
+<div class="container col-12 col-md-12 d-flex flex-column flex-md-row justify-content-between mt-1 bg-body-tertiary">
+
+    <p>dette er en test for at se om den kommer på github</p>
+</div>
+
+
+
+<?php /*
+<div class="row g-2">
+	<?php
+	$produkter = $db->sql("SELECT * FROM produkter");
+	foreach($produkter as $produkt) {
+		?>
+		<div class="col-12 col-md-6">
+			<div class="card w-100">
+				<div class="card-header">
+					<?php
+					echo $produkt->prodNavn;
+					?>
+				</div>
+                <div class="card-body">
+                    <?php
+                    echo '<img src="data:image/jpeg;base64,' . base64_encode($produkt->prodBillede) . '" alt="Produkt billede" style="max-width:100%; height: auto;">';
+                    ?>
+                </div>
+
+				<div class="card-body">
+					<?php
+					// Indsæt andet felt fra database
+                    echo $produkt->prodBeskrivelse;
+					?>
+				</div>
+				<div class="card-footer text-muted">
+					<?php
+					// Indsæt andet felt fra database
+                    echo number_format($produkt->prodPris, 2, ',', '.'). " DKK";
+					?>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
+	?>
+</div>
+*/?>
+
+<?php include 'settings/buttomscripts.php';
+include 'settings/footer.php';?>
+</body>
+</html>
