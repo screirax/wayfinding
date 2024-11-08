77978b2a601c7abdabf499639ef8d25abf4aad2b
screirax <eqwow@hotmail.com>
2024-11-08 09:07:31 +0100

    test2

--- rummaal.php
+++ rummaal.php
@@ -27,18 +27,23 @@ require "settings/init.php";
 
 <div class="container col-12 col-md-12 d-flex flex-column flex-md-row justify-content-between mt-1 bg-body-tertiary">
 
+    <div class="col-12 col-md-6 mt-5">
+        <h2 class="text-left">Vælg en væg og skriv dine mål</h2>
+        
+        <div class="input-group mb-3 w-50 mt-4">
+            <span class="input-group-text" id="basic-addon1">Højde</span>
+            <input type="text" class="form-control" placeholder="Centimeter" aria-label="height" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group mb-3 w-50 mt-4">
+            <span class="input-group-text" id="basic-addon1">Bredde</span>
+            <input type="text" class="form-control" placeholder="Centimeter" aria-label="width" aria-describedby="basic-addon1">
+        </div>
+    </div>
+
     <div class="mt-5 col-12 col-md-6">
         <img class="img-fluid" src="pictures/aabning-nr40.webp">
     </div>
 
-
-    <div class="col-12 col-md-6 mt-5">
-        <h1 class="text-left">Prøv vores nye program til at oprette dit nye rum.</h1>
-        <p class="text-left mt-1">
-            Man kan fremad se, at de har været udset til at læse, at der skal dannes par af ligheder. Dermed kan der afsluttes uden løse ender, og de kan optimeres fra oven af at formidles stort uden brug fra presse. I en kant af landet går der blandt om, at de vil sætte den over forbehold for tiden. Vi flotter med et hold, der vil rundt og se sig om i byen. Det gør heller ikke mere. Men hvor vi nu overbringer denne størrelse til det den handler om, så kan der fortælles op til 3 gange. Hvis det er træet til dit bord der får dig op, er det snarere varmen over de andre.
-        </p>
-
-    </div>
 </div>
 
 
